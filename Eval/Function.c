9fbf13fb34145f24c478cc69e4263222a19d66e6
Chloe <chloe.merck@ynov.com>
2020-02-19 10:23:07 +0100

    first commit

---
+++ Eval/Function.c
@@ -0,0 +1,9 @@
+//
+//  Function.c
+//  Eval
+//
+//  Created by Chloé Merck on 17/02/2020.
+//  Copyright © 2020 Chloé Merck. All rights reserved.
+//
+
+#include "Function.h"
---
+++ Eval/Function.h
@@ -0,0 +1,14 @@
+//
+//  Function.h
+//  Eval
+//
+//  Created by Chloé Merck on 17/02/2020.
+//  Copyright © 2020 Chloé Merck. All rights reserved.
+//
+
+#ifndef Function_h
+#define Function_h
+
+#include <stdio.h>
+
+#endif /* Function_h */
