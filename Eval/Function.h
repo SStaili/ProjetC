56a659ce4ddc0962353b420f24d494bbd16de278
ChloeMrk <56391670+ChloeMrk@users.noreply.github.com>
2020-03-11 11:36:43 +0100

    Ajout commentaire

--- Eval/Function.h
+++ Eval/Function.h
@@ -11,8 +11,15 @@
 
 #include <stdio.h>
 
-int AffichagemMenu(void);
+int AffichagemMenu(void); //Chloe Merck
 
-void Etage1(int*tab, int tailleTableau);
+void Etage1(int*tab, int tailleTableau); //Jean Baptiste
+
+
+int Etage1(void); //Chloe Merck
+
+int Recherche(void); //Chloe Merck
+
+void NomPersonnage(Personnage *p); //Chloe Merck
 
 #endif /* Function_h */
