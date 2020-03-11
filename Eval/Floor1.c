92db6eebde2e3c81b49d60a1e736b07668efcbca
ChloeMrk <56391670+ChloeMrk@users.noreply.github.com>
2020-03-11 11:32:55 +0100

    Ajout commentaire

--- Eval/Floor1.c
+++ Eval/Floor1.c
@@ -9,9 +9,27 @@
 #include "Floor1.h"
 #include "ennemi.c"
 
-int floor1ennemi(ennemi1,)
+int floor1ennemi(ennemi1,) //Jean Baptiste
 {
     //call function ennemi1 ( atk,def,vie,)
     printf("Bienvenue ");
  //combat...
+}
+
+int Etage1(){ //Chloe Merck
+
+    int A[10][10] = { { }};
+    int I,J;
+
+    
+    for(I=0; I<6; I++)
+    {
+      
+        for(J=0; J<10; J++)
+        {
+            printf("%7d", A[I][J]);
+        }
+    }
+    printf("\n");
+        return 0;
 }
